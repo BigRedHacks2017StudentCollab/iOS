561d0bb613ab4567ddec05b676f83f462bb6348e
Mohamed Yassin <mohamed.aladin.yassin@gmail.com>
2017-09-16 17:58:15 -0400

    Firebase pulling data, tableview sync, some design

--- StudentCollab/RoomJoinerViewController.swift
+++ StudentCollab/RoomJoinerViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class RoomJoinerViewController: UIViewController {
+    var ref: FIRDatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = FIRDatabase.database().reference()
         initialize();
     }
 
@@ -37,6 +39,10 @@ class RoomJoinerViewController: UIViewController {
         // Firebase listener, this will trigger an update to the room listener
         // This will be using firebase Real time database. Take a look at the
         // Docs
+        refHandle = ref.child("rooms").observe(DataEventType.value, with: { (snapshot) in
+            let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+            // ...
+        })
     }
     
     func createButtonDialogInit() {
@@ -55,10 +61,17 @@ class Room {
     var host: String = "";
     var status: String = "inactive";
     var students = [Student]();
+    init(host:String, status:String){
+        self.host = host;
+        self.status = status;
+    }
 }
 
 class Student {
     var name : String = ""
     var answered : Int = 0
     var correct : Int = 0
+    init(name:String) {
+        self.name = name;
+    }
 }
--- StudentCollab/RoomsViewController.swift
+++ StudentCollab/RoomsViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import Firebase
 
-class RoomsViewController:  UIViewController, UITableViewDataSource {
-    
-    
-    var rooms = [Room]();
-    
-    
+class RoomsViewController: UIViewController, UITableViewDataSource{
+
+    var ref: DatabaseReference!
     @IBOutlet weak var roomsTableView: UITableView!
+    var roomsDatabaseListener : UInt!
+    var rooms = [Room]();
+
+
+    // Table View Boiler Plate
+    // How many sections, since its going to only be one just do one.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    // Return Int, how many rows in the table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rooms.count
+    }
+
+    // Called every time for each row
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.roomsTableView.dequeueReusableCell(withIdentifier: "CustomTableCell") as? RoomsTableViewCell
+        let room = self.rooms[indexPath.row]
+        
+        cell?.labelText.text = room.name
+        return cell!
+    }
     
+
+
+
+
+
     override func viewDidLoad() {
+        ref = Database.database().reference()
         super.viewDidLoad()
         initialize();
+        roomsTableView.dataSource = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,55 +56,28 @@ class RoomsViewController:  UIViewController, UITableViewDataSource {
     
     
     func initialize() {
-        tableViewInit();
         roomListenerInit();
         createButtonDialogInit();
     }
-    
-    
-    
-    
-    
-    
-    func tableViewInit() {
-        // Boilerplate for the table view for stuff like amount of items etc.
-        // Probaly should create a class because it will get somewhat messy and
-        // ugly.
-        
-        // Register the table view cell class and its reuse id
-        
-        // This view controller itself will provide the delegate methods and row data for the table view.
-        
-    }
-    
-    // How many sections, since its going to only be one just do one.
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    // Return Int, how many rows in the table
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.rooms.count
-    }
-    
-    // Called every time for each row
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.roomsTableView.dequeueReusableCell(withIdentifier: "Cell") as? RoomsTableViewCell
-        let room = self.rooms[indexPath.row]
-        
-        cell?.labelText.text = room.name
-        return cell!
-    }
-    
-    
-    
-    
-    
-    
+
     func roomListenerInit() {
         // Firebase listener, this will trigger an update to the room listener
         // This will be using firebase Real time database. Take a look at the
         // Docs
+        roomsDatabaseListener = ref.child("rooms").observe(DataEventType.value, with: { (snapshot) in
+            if !snapshot.exists() { return }
+            let enumerator = snapshot.children
+            while let roomSnap = enumerator.nextObject() as? DataSnapshot {
+                guard let room = roomSnap.value as? [String: Any] else { continue }
+                let status = room["status"] as! String
+                if(status == "active" ) {
+                    let newRoom : Room = Room(name: room["name"] as! String, host: room["host"] as! String, status: room["status"] as! String);
+                    self.rooms.append(newRoom)
+                }
+            }
+
+                self.roomsTableView.reloadData()
+        })
     }
     
     func createButtonDialogInit() {
@@ -92,14 +93,22 @@ class RoomsViewController:  UIViewController, UITableViewDataSource {
 }
 
 class Room {
-    var name : String = "";
+    var name: String = ""
     var host: String = "";
     var status: String = "inactive";
     var students = [Student]();
+    init(name:String,host:String, status:String){
+        self.name = name
+        self.host = host;
+        self.status = status;
+    }
 }
 
 class Student {
     var name : String = ""
     var answered : Int = 0
     var correct : Int = 0
+    init(name:String) {
+        self.name = name;
+    }
 }
