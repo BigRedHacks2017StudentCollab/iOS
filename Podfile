b6da7e0e10562a812fc5064f8971307c6c006dba
Mohamed Yassin <mohamed.aladin.yassin@gmail.com>
2017-09-16 12:55:40 -0400

    Initial frame; Firebase,SCL alert and boilerplate

--- Podfile
+++ Podfile
@@ -4,6 +4,8 @@
 target 'StudentCollab' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
+  pod 'SCLAlertView'
+  pod 'Firebase/Database’
 
   # Pods for StudentCollab
 
---
+++ StudentCollab/RoomJoinerViewController.swift
@@ -0,0 +1,64 @@
+//
+//  RoomJoinerViewController.swift
+//  StudentCollab
+//
+//  Created by Mohamed Yassin on 9/16/17.
+//  Copyright © 2017 Mohamed Yassin. All rights reserved.
+//
+
+import UIKit
+
+class RoomJoinerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialize();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    func initialize() {
+        tableViewInit();
+        roomListenerInit();
+        createButtonDialogInit();
+    }
+    
+    func tableViewInit() {
+        // Boilerplate for the table view for stuff like amount of items etc.
+        // Probaly should create a class because it will get somewhat messy and
+        // ugly.
+        
+    }
+    
+    func roomListenerInit() {
+        // Firebase listener, this will trigger an update to the room listener
+        // This will be using firebase Real time database. Take a look at the
+        // Docs
+    }
+    
+    func createButtonDialogInit() {
+        // Dialog setup for when the create button is clicked
+        // Using SCL Alert View
+    }
+
+    @IBAction func NewRoomButton(_ sender: UIButton) {
+        // Event when the plus button is clicked
+        // It should call a showing of the createButtonDialog
+    }
+
+}
+
+class Room {
+    var host: String = "";
+    var status: String = "inactive";
+    var students = [Student]();
+}
+
+class Student {
+    var name : String = "name"
+    var answered : Int = 0
+    var correct : Int = 0
+}
